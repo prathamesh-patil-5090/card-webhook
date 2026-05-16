0f361cd5c5fc0900fffb295258b1977275e4a87e
Prathamesh Patil <122prathamesh6002@sjcem.edu.in>
2026-05-16 23:26:34 +0530

    fix: allowed all cors

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,24 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["puppeteer-core", "@sparticuz/chromium-min"],
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,POST,PUT,PATCH,DELETE",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "X-Requested-With, Content-Type, Accept, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
